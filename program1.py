b7a7e40d6a1c4b30c41bf9df3123bddb3ae10766
Saurabh12770 <159509415+Saurabh12770@users.noreply.github.com>
2024-10-08 15:52:42 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,19 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+def is_valid_parentheses(s: str) -> bool:
+    matching_brackets={')' : '(','}': '{', ']' : '['}
+    stack=[]
+
+    for char in s:
+        if char in matching_brackets:
+            top_element=stack.pop() if stack else '#'
+            if matching_brackets[char] != top_element:
+                return false
+        else:
+            stack.append(char)
+
+     return not stack
+
+# example
+#print(is_valid_parentheses("()"))
 
 
 
